70a5fb90d7327503303e42ee871746bf9e517762
Leo Banos Garcia <bsleo625@gmail.com>
2023-06-19 16:28:51 -0600

    Adds challenge for boolean expressions to make decisions

--- CoinFlip.cs
+++ CoinFlip.cs
@@ -1,4 +1,4 @@
-Random flip = new Random();
-int coin = flip.Next(2);
+// Random flip = new Random();
+// int coin = flip.Next(2);
 
-Console.WriteLine((coin == 0) ? "heads" : "tails");
+// Console.WriteLine((coin == 0) ? "heads" : "tails");
