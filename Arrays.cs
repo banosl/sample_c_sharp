d930a412b90ca3ccd18b547a61ece3a30f2974bc
Leo Banos Garcia <bsleo625@gmail.com>
2023-06-19 09:49:17 -0600

    fraudulent orders challenge where I ran a foreach to check which ID started with 'b'

--- Arrays.cs
+++ Arrays.cs
@@ -16,14 +16,14 @@
 //   Console.WriteLine(name);
 // }
 
-int[] inventory = { 200, 450, 700, 175, 250 };
-int sum = 0;
-int bin = 0;
-foreach (int item in inventory)
-{
-  sum += item;
-  bin++;
-  Console.WriteLine($"Bin {bin} = {item} (Running total: {sum})");
-}
+// int[] inventory = { 200, 450, 700, 175, 250 };
+// int sum = 0;
+// int bin = 0;
+// foreach (int item in inventory)
+// {
+//   sum += item;
+//   bin++;
+//   Console.WriteLine($"Bin {bin} = {item} (Running total: {sum})");
+// }
 
-Console.WriteLine($"We have {sum} items in inventory.");
+// Console.WriteLine($"We have {sum} items in inventory.");
