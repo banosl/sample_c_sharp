ac3fa1c9d6cb06f3243bb7310b6ede17ac5ea699
Leo Banos Garcia <bsleo625@gmail.com>
2023-06-19 09:30:21 -0600

    Adds basic example of array

--- subscriptionRenewal.cs
+++ subscriptionRenewal.cs
@@ -1,28 +1,28 @@
-Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+// Random random = new Random();
+// int daysUntilExpiration = random.Next(12);
+// int discountPercentage = 0;
 
-if (daysUntilExpiration <= 10)
-{
-  if (daysUntilExpiration <= 5)
-  {
-    if (daysUntilExpiration == 1)
-    {
-      Console.WriteLine("Your Subscription expires within a day!");
-      Console.WriteLine($"Renew now and save {discountPercentage += 20}%!");
-    }
-    else if (daysUntilExpiration == 0)
-    {
-      Console.WriteLine("Your subscription has expired");
-    }
-    else
-    {
-      Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
-      Console.WriteLine($"Renew now and save {discountPercentage += 10}%!");
-    }
- }
-else
-  {
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-  }
-}
+// if (daysUntilExpiration <= 10)
+// {
+//   if (daysUntilExpiration <= 5)
+//   {
+//     if (daysUntilExpiration == 1)
+//     {
+//       Console.WriteLine("Your Subscription expires within a day!");
+//       Console.WriteLine($"Renew now and save {discountPercentage += 20}%!");
+//     }
+//     else if (daysUntilExpiration == 0)
+//     {
+//       Console.WriteLine("Your subscription has expired");
+//     }
+//     else
+//     {
+//       Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+//       Console.WriteLine($"Renew now and save {discountPercentage += 10}%!");
+//     }
+//  }
+// else
+//   {
+//     Console.WriteLine("Your subscription will expire soon. Renew now!");
+//   }
+// }
