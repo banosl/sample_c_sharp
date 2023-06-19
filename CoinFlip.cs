83887df128745215a13268913a6a88bb5b2e1147
Leo Banos Garcia <bsleo625@gmail.com>
2023-06-19 16:15:55 -0600

    Adds example of conditional operator

---
+++ CoinFlip.cs
@@ -0,0 +1,4 @@
+Random flip = new Random();
+int coin = flip.Next(2);
+
+Console.WriteLine((coin == 0) ? "heads" : "tails");
--- Comments.cs
+++ Comments.cs
@@ -2,21 +2,21 @@
 // and turns it back into a string. It prints that reversed string and a message with how
 // many times 'o' appears in it.
 
-string story = "The quick brown fox jumps over the lazy dog.";
-char[] storyCharacters = story.ToCharArray();
-int appearance = 0;
+// string story = "The quick brown fox jumps over the lazy dog.";
+// char[] storyCharacters = story.ToCharArray();
+// int appearance = 0;
 
-Array.Reverse(storyCharacters);
+// Array.Reverse(storyCharacters);
 
-foreach (char letter in storyCharacters) 
-{ 
-  if (letter == 'o') 
-    { 
-      appearance++; 
-    } 
-}
+// foreach (char letter in storyCharacters) 
+// { 
+//   if (letter == 'o') 
+//     { 
+//       appearance++; 
+//     } 
+// }
 
-string newMessage = new String(storyCharacters);
+// string newMessage = new String(storyCharacters);
 
-Console.WriteLine(newMessage);
-Console.WriteLine($"'o' appears {appearance} times.");
+// Console.WriteLine(newMessage);
+// Console.WriteLine($"'o' appears {appearance} times.");
