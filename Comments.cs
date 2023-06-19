aa842ec63a7638e41baab7ff064fe59d506609b9
Leo Banos Garcia <bsleo625@gmail.com>
2023-06-19 10:58:01 -0600

    added some corrections to match naming and commenting conventions

---
+++ Comments.cs
@@ -0,0 +1,22 @@
+// This code takes a story, turns it into an array of characters, reverses that array
+// and turns it back into a string. It prints that reversed string and a message with how
+// many times 'o' appears in it.
+
+string story = "The quick brown fox jumps over the lazy dog.";
+char[] storyCharacters = story.ToCharArray();
+int appearance = 0;
+
+Array.Reverse(storyCharacters);
+
+foreach (char letter in storyCharacters) 
+{ 
+  if (letter == 'o') 
+    { 
+      appearance++; 
+    } 
+}
+
+string newMessage = new String(storyCharacters);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {appearance} times.");
--- FraudulentOrders.cs
+++ FraudulentOrders.cs
@@ -1,9 +1,9 @@
-string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+// string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
 
-foreach(string id in orderIDs)
-{
-  if (id.StartsWith("B"))
-  {
-    Console.WriteLine($"Order {id} starts with 'B'!");
-  }
-}
+// foreach(string id in orderIDs)
+// {
+//   if (id.StartsWith("B"))
+//   {
+//     Console.WriteLine($"Order {id} starts with 'B'!");
+//   }
+// }
