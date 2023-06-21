92bcbb76c245f4fbee0debd2ecdb6914bdf634e2
Leo Banos Garcia <bsleo625@gmail.com>
2023-06-21 11:25:01 -0600

    Adds examples for do-while, while, and continue statements

---
+++ DoWhileWhile.cs
@@ -0,0 +1,31 @@
+Random random = new Random();
+// int current = 0;
+int current = random.Next(1, 11);
+
+// do-while statement
+
+// do
+// {
+//   current = random.Next(1, 11);
+//   Console.WriteLine(current);
+// } while (current != 7);
+
+// // while
+// while (current >= 3)
+// {
+//   Console.WriteLine(current);
+//   current = random.Next(1, 11);
+// }
+
+// Console.WriteLine($"Last number: {current}");
+
+//Continue Statement
+
+// do
+// {
+//   current = random.Next(1, 11);
+
+//   if (current >= 8) continue;
+
+//   Console.WriteLine(current);
+// } while (current != 7);
--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -1,19 +1,19 @@
-for(int number = 1; number <= 100; number++)
-{
-  if ((number % 5) == 0 && (number % 3) == 0)
-  {
-    Console.WriteLine(number + " - FizzBuzz");
-  } 
-  else if ((number % 5) == 0)
-  {
-    Console.WriteLine(number + " - Buzz");
-  }
-  else if ((number % 3) == 0)
-  {
-    Console.WriteLine(number + " - Fizz");
-  }
-  else
-  {
-    Console.WriteLine(number);
-  }
-}
+// for(int number = 1; number <= 100; number++)
+// {
+//   if ((number % 5) == 0 && (number % 3) == 0)
+//   {
+//     Console.WriteLine(number + " - FizzBuzz");
+//   } 
+//   else if ((number % 5) == 0)
+//   {
+//     Console.WriteLine(number + " - Buzz");
+//   }
+//   else if ((number % 3) == 0)
+//   {
+//     Console.WriteLine(number + " - Fizz");
+//   }
+//   else
+//   {
+//     Console.WriteLine(number);
+//   }
+// }
