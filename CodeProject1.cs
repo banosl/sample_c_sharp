d75e3cb44b7ad998a098f4a2824f0928b3b3c48e
Leo Banos Garcia <bsleo625@gmail.com>
2023-06-21 17:43:37 -0600

    Adds CodeProject2

--- CodeProject1.cs
+++ CodeProject1.cs
@@ -1,28 +1,28 @@
-Console.WriteLine("Enter an integer value between 5 and 10");
-bool check = false;
-var input = Console.ReadLine(); 
+// Console.WriteLine("Enter an integer value between 5 and 10");
+// bool check = false;
+// var input = Console.ReadLine(); 
 
-do
-{
-  if (Char.IsDigit(input, 0))
-  {
-    int number = Convert.ToInt32(input);
-    if(number < 5 || number > 10)
-    {
-      Console.WriteLine($"You entered {input}. Please enter a number between 5 and 10.");
-      input = Console.ReadLine(); 
-    }
-    else
-    {
-      check = true;
-    }
-  }
-  else  
-  {
-    Console.WriteLine($"You entered {input}. Please enter a valid number.");
-    input = Console.ReadLine(); 
-  }
+// do
+// {
+//   if (Char.IsDigit(input, 0))
+//   {
+//     int number = Convert.ToInt32(input); //Fails here because a user could enter a non number char after the first number.
+//     if(number < 5 || number > 10)
+//     {
+//       Console.WriteLine($"You entered {input}. Please enter a number between 5 and 10.");
+//       input = Console.ReadLine(); 
+//     }
+//     else
+//     {
+//       check = true;
+//     }
+//   }
+//   else  
+//   {
+//     Console.WriteLine($"You entered {input}. Please enter a valid number.");
+//     input = Console.ReadLine(); 
+//   }
 
-} while(check == false);
+// } while(check == false);
 
-Console.WriteLine($"Your input value ({input}) has been accepted.");
+// Console.WriteLine($"Your input value ({input}) has been accepted.");
