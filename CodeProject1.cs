9d1e35a24ffa5b8eb77e4e8254cd9f6f08a5b465
Leo Banos Garcia <bsleo625@gmail.com>
2023-06-21 16:34:29 -0600

    Fix an issue with input

--- CodeProject1.cs
+++ CodeProject1.cs
@@ -1,16 +1,16 @@
 Console.WriteLine("Enter an integer value between 5 and 10");
 bool check = false;
-string input;
+var input = Console.ReadLine(); 
+
 do
 {
-  input = Console.ReadLine(); //This breaks if a user doesn't enter anything or enters a non number after the first number like "1t"
-
   if (Char.IsDigit(input, 0))
   {
     int number = Convert.ToInt32(input);
     if(number < 5 || number > 10)
     {
       Console.WriteLine($"You entered {input}. Please enter a number between 5 and 10.");
+      input = Console.ReadLine(); 
     }
     else
     {
@@ -20,6 +20,7 @@ do
   else  
   {
     Console.WriteLine($"You entered {input}. Please enter a valid number.");
+    input = Console.ReadLine(); 
   }
 
 } while(check == false);
---
+++ CodeProject2.cs
@@ -0,0 +1,1 @@
+// var input = Console.ReadLine();
