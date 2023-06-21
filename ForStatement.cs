566c1915d5bef16b83b9fdba590e1a6b053724a8
Leo Banos Garcia <bsleo625@gmail.com>
2023-06-21 10:53:36 -0600

    Adds for statement examples

---
+++ ForStatement.cs
@@ -0,0 +1,21 @@
+// for (int i = 10; i >= 0; i--)
+// {
+//   Console.WriteLine(i);
+//   if (i == 7) break;
+// }
+
+//looping backwards through an array
+
+// string[] names = { "Alex", "Eddie", "David", "Michael" };
+// for (int i = names.Length - 1; i >= 0; i--)
+// {
+//   Console.WriteLine(names[i]);
+// }
+
+//looping through an array and replacing a value with an if statement
+
+// string[] names = { "Alex", "Eddie", "David", "Michael" };
+// for (int i = 0; i < names.Length; i++)
+//     if (names[i] == "David") names[i] = "Sammy";
+
+// foreach (var name in names) Console.WriteLine(name);
