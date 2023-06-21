05050e1f083fa6d661e83f1a30ac7d98eea69254
Leo Banos Garcia <bsleo625@gmail.com>
2023-06-21 10:12:08 -0600

    Adds example of switch-case construct

--- VariableCodeBlock.cs
+++ VariableCodeBlock.cs
@@ -9,18 +9,19 @@
 // {
 //   Console.WriteLine(flag);
 // }
+// using System;
 
-int[] numbers = { 4, 8, 15, 16, 23, 42 };
-int total = 0;
-bool found = false;
+// int[] numbers = { 4, 8, 15, 16, 23, 42 };
+// int total = 0;
+// bool found = false;
 
-foreach (int number in numbers)
-{
-  total += number;
-  if (number == 42)
-    found = true;
-}
+// foreach (int number in numbers)
+// {
+//   total += number;
+//   if (number == 42)
+//     found = true;
+// }
 
-if (found) Console.WriteLine("Set contains 42");
+// if (found) Console.WriteLine("Set contains 42");
 
-Console.WriteLine($"Total: {total}");
+// Console.WriteLine($"Total: {total}");
